3af9aa83bdf3e2a0946b7a15685f5d455aaf01d7
unknown <matthew.j.alioto@gmail.com>
2014-08-10 22:43:55 -0400

    init

---
+++ wikiqueue.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name		WikiQueue
+// @namespace	malioto
+// @description	Wiki link queuing system. Keep yourself distracted.
+// @match		http://*/*
+// @resource	https://cdn1.iconfinder.com/data/icons/socialmediaicons_v120/24/wikipedia.png
+// @updateURL	https://github.com/mja2637/wikiqueue/wikiqueue.meta.js
+// @version		0.1
+// ==/UserScript==
